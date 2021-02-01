342d2f00df3c176f789e05f53ca7623d48537ba5
Frederik Knudsen <frederik.s.k@hotmail.com>
2021-02-01 12:42:10 +0100

    Added functions

--- GlassSegmentV1/GlassSegmentV1.py
+++ GlassSegmentV1/GlassSegmentV1.py
@@ -1,2 +1,20 @@
+import cv2
 print("hej")
-print("hej igen")
+print(("hej igen"))
+print(("hej igen igen"))
+bla = 2
+if (bla + (bla+2)) == 4:
+    print("hej 2 er 4")
+else:
+    print("NEJ")
+
+
+def func(tal, bogstave):
+    if tal > 5:
+        print("tal stor")
+    else:
+        print(bogstave)
+    return bogstave
+
+def func2(tal2, bogstave2):
+    return bogstave2
