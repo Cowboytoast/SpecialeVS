8aeb959a40b43e732a651baa7330010b2e1663a5
Frederik Knudsen <frederik.s.k@hotmail.com>
2021-02-25 01:25:53 +0100

    Added a lot of "template matching" stuff

--- GlassSegmentV1/GlassSegmentV1.py
+++ GlassSegmentV1/GlassSegmentV1.py
@@ -2,13 +2,9 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-
-# * Do simple morphology to remove noise from image
-def RemoveNoise(img, dim = 3):
-    kernel = np.ones((dim, dim))
-    img_closed = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
-    img_opened = cv2.morphologyEx(img_closed, cv2.MORPH_OPEN, kernel)
-    return img_opened
+import math
+import imutils
+from scipy import ndimage
 
 # * Resize image to fit screen while keeping aspect ratio
 def ResizeToFit(oriimg):
@@ -26,51 +22,51 @@ def ResizeToFit(oriimg):
     newimg = cv2.resize(oriimg,(int(newX),int(newY)))
     return newimg
 
-def ContourImage(img):
-    contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    drawing = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
-    for i in range(len(contours)):
-        if contours[i].size > 50:
-            cv2.drawContours(drawing, contours, i, (255, 255, 255), 2, cv2.LINE_8, hierarchy, 0)
-    drawing = cv2.cvtColor(drawing, cv2.COLOR_BGR2GRAY)
-    return drawing
-
+# ! Unused due to bounding-box non-scaliness. Use rotate_bound instead
 def rotate_image(image, angle):
     image_center = tuple(np.array(image.shape[1::-1]) / 2)
     rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
-    result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+    result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_CUBIC)
     return result
 
-# * Load image and resize
-img = cv2.imread('IMG_0005_cropped.png')
-img_screensized = ResizeToFit(img)
+# ! A (likely) ineffective way to convert image to binary
+def cvt_to_bin(image):
+    image_bin = np.zeros(image.shape,dtype=bool)
+    for y in range(image.shape[0]):
+        for x in range(image.shape[1]):
+            if image[y,x] == 255:
+                image_bin[y,x] = True
+    return image_bin
 
-# * Template stuff
+# * Load image and convert to binary
+img = cv2.imread('SimpleSegmentation.png', 0)
+img_bin = cvt_to_bin(img)
+
+# * Load template and convert to binary
 template = cv2.imread('VialOutline.png', 0)
-scale_pct = 33.585 #33.585
-template_width, template_height = template.shape[::-1]
-template_width = int(template.shape[1] * scale_pct / 100)
-template_height = int(template.shape[0] * scale_pct / 100)
-dsize = (template_height, template_height)
-template = cv2.resize(template, dsize)
+template_bin = cvt_to_bin(template)
 
-# * Detect edges on image and remove noise
-img_blurred = cv2.blur(img_screensized, (5,5))
-cv2.imshow('filtered', img_blurred)
-edges = cv2.Canny(img_blurred, 45, 45)
-edges_lownoise = RemoveNoise(edges, 5)
+# * Rotating template in 15 deg. increments
+# * and match with image patch
+angle_inc = 15 # 15 deg. increment
+template_rot = template
+matches = np.empty([1000, 2000])
+# ? Perhaps scaling down the image significantly would 
+# ? increase performance. But at what cost?
 
-# * Rotating template in 5 deg. increments
-angle_inc = 5 # 5 deg. increment
-for i in range(0, int(360/angle_inc)):
-    template = rotate_image(template, angle_inc)
-    res = cv2.matchTemplate(edges_lownoise, template, cv2.TM_CCORR_NORMED)
-    threshold = 0.4
-    loc = np.where(res >= threshold)
-    for pt in zip(*loc[::-1]):
-        cv2.rectangle(img_screensized, pt, (pt[0] + template_width, pt[1] + template_height), (0,0,255), 2)
-
-cv2.imshow('Matches', img_screensized)
-cv2.imshow('Contour image', edges_lownoise)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# Do & operation in increments, that is moving the template image a few pixels right/down
+# for each iteration and store most pixel hits
+# TODO: Extract sum of Trues for each width inc. and save in new array
+    # TODO: Get array instead of single value when &'ing arrays
+for angle in np.arange(0, 360, angle_inc):
+    for h in np.arange(0, img.shape[0] - template_rot.shape[0], int(img.shape[0] / 25)):
+        for w in np.arange(0, img.shape[1] - template_rot.shape[1], int(img.shape[1] / 25)):       
+            match_array = np.logical_and(img[h : h + template_rot.shape[0] : 1, w : w + template_rot.shape[1] : 1], template_rot)
+            matches[h, w] = np.count_nonzero(match_array)
+    template_rot = imutils.rotate_bound(template, angle)
+print(np.amax(match_array))
+time.sleep(1)
+#cv2.imshow('Binary', img)
+#cv2.imshow('Contour image', edges_lownoise)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
