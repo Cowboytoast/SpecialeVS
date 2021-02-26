3b1b02cfada889ead34c0b8689520336d0f0ef99
Frederik Knudsen <frederik.s.k@hotmail.com>
2021-02-26 14:58:44 +0100

    Added functional correspondance matching

--- GlassSegmentV1/GlassSegmentV1.py
+++ GlassSegmentV1/GlassSegmentV1.py
@@ -9,7 +9,7 @@ from scipy import ndimage
 # * Resize image to fit screen while keeping aspect ratio
 def ResizeToFit(oriimg):
     H, W = (980, 1820)
-    height, width, depth = oriimg.shape
+    height, width = oriimg.shape
     scaleWidth = float(W)/float(width)
     scaleHeight = float(H)/float(height)
 
@@ -38,38 +38,50 @@ def cvt_to_bin(image):
                 image_bin[y,x] = True
     return image_bin
 
+def templatematch(img, template, angle_inc = 5, h_steps = 25, w_steps = 40):
+    template_rot = template
+    matches = np.empty([int(360/angle_inc), img.shape[0] - template_rot.shape[1] + 1, img.shape[1] - template_rot.shape[1] + 1])
+    #rotations = np.empty([])
+    # ? Perhaps scaling down the image significantly would 
+    # ? increase performance. But at what cost?
+
+    # Do & operation in increments, that is moving the template image a few pixels right/down
+    # for each iteration and store most pixel hits
+    for angle in np.arange(0, 360, angle_inc):
+        for h in np.arange(0, img.shape[0] - template_rot.shape[0], int(img.shape[0] / h_steps)):
+            for w in np.arange(0, img.shape[1] - template_rot.shape[1], int(img.shape[1] / w_steps)):
+                match_array = np.logical_and(img[h : h + template_rot.shape[0] : 1, w : w + template_rot.shape[1] : 1], template_rot)
+                matches[int(angle/angle_inc), h, w] = np.count_nonzero(match_array)
+        template_rot = imutils.rotate_bound(template, angle)
+
+    max_idx = np.where(matches == np.amax(matches))
+    max_h = np.amax(max_idx[1])
+    max_w = np.amax(max_idx[2])
+    detection = imutils.rotate_bound(template, np.amax(max_idx[0]) * angle_inc)
+    final = img
+    final[max_h:max_h + detection.shape[0]:1, max_w:max_w+detection.shape[1]:1] += detection
+    return final
+
 # * Load image and convert to binary
-img = cv2.imread('SimpleSegmentation.png', 0)
-img_bin = cvt_to_bin(img)
+img = cv2.imread('IMG_0005_opt3.png', 0)
+img = ResizeToFit(img)
+#img_bin = cvt_to_bin(img)
 
 # * Load template and convert to binary
-template = cv2.imread('VialOutline.png', 0)
-template_bin = cvt_to_bin(template)
+template = cv2.imread('VialOutlineHollow.png', 0)
 
-# * Rotating template in 15 deg. increments
+# * Rotating template in increments
 # * and match with image patch
-angle_inc = 5 # 15 deg. increment
-template_rot = template
-matches = np.empty([1000, 2000])
-# ? Perhaps scaling down the image significantly would 
-# ? increase performance. But at what cost?
+final = templatematch(img, template, angle_inc = 5, w_steps = 40, h_steps = 25)
 
-# Do & operation in increments, that is moving the template image a few pixels right/down
-# for each iteration and store most pixel hits
-# TODO: Extract sum of Trues for each width inc. and save in new array
-    # TODO: Get array instead of single value when &'ing arrays
-for angle in np.arange(0, 360, angle_inc):
-    for h in np.arange(0, img.shape[0] - template_rot.shape[0], int(img.shape[0] / 25)):
-        for w in np.arange(0, img.shape[1] - template_rot.shape[1], int(img.shape[1] / 25)):
-            #i = 0       
-            match_array = np.logical_and(img[h : h + template_rot.shape[0] : 1, w : w + template_rot.shape[1] : 1], template_rot)
-            matches[h, w] = np.count_nonzero(match_array)
-    template_rot = imutils.rotate_bound(template, angle)
-    #cv2.imshow('bla',template_rot)
-    #cv2.waitKey(50)
-print(np.amax(match_array))
-time.sleep(1)
+# TODO Prepare to function with line coordinates to narrow down position
+# TODO Function with Mathias' program which sends:
+# TODO line-pair = |slope1 = a rad | x1start | y1start | x1end | y1end | slope2 = b rad | x2start | y2start | x2end | y2end
+# TODO Search in the point-wise vicinity, search within , e.g +- 10 deg, turn 180 deg, search again within +-10 deg, use best fit
+cv2.imshow('bla1', img)
+cv2.imshow('bla', final)
+
 #cv2.imshow('Binary', img)
 #cv2.imshow('Contour image', edges_lownoise)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
