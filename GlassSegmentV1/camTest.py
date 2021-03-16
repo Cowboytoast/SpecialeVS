94259de04c68664f5186663377d3df96f3c44d1c
MathiasBadstue <s153096@student.dtu.dk>
2021-03-16 15:05:49 +0100

    Added camera testing

--- GlassSegmentV1/camTest.py
+++ GlassSegmentV1/camTest.py
@@ -1,1 +1,33 @@
 #TODO: Indfør kamera kode.
+import cv2
+
+#cam = cv2.VideoCapture(1)
+cam = cv2.VideoCapture("rtsp://[Mathias Badstue]:[Campus2020]@[99.83.164.242]/video_feed")
+
+
+cv2.namedWindow("test")
+
+img_counter = 0
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to grab frame")
+        break
+    cv2.imshow("test", frame)
+
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        # SPACE pressed
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+
+cam.release()
+
+cv2.destroyAllWindows()
