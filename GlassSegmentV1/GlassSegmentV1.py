0992b23183478ac17164e9b0695b72b28d199384
Cowboytoast <frederik.s.k@hotmail.com>
2021-03-22 15:29:11 +0100

    Added function libraries and cleanup

--- GlassSegmentV1/GlassSegmentV1.py
+++ GlassSegmentV1/GlassSegmentV1.py
@@ -1,319 +1,15 @@
 import cv2
-import matplotlib.pyplot as plt
 import time
-import pylab
-from scipy import ndimage
-from skimage.filters import difference_of_gaussians
+import LineSearchLib as ls
+import PreprocessingLib as prep
 from skimage.util import img_as_ubyte
-from matplotlib import cm
 import numpy as np
-from scipy.stats import linregress
-from skimage.transform import hough_line
 
 #*********** GLOBAL PARAMETERS **************
 angleTolerance = 0.3
 
 #********************************************
 
-# * Histogram stretching function
-def HistStretch(img):
-    tmp = img.copy()
-    maxval = tmp.max()
-    minval = tmp.min()
-    for height in range(img.shape[0]):
-        for width in range(img.shape[1]):
-            tmp[height, width] = ((tmp[height, width] - minval) / (maxval - minval)) * 255
-    return tmp
-
-def DiffOfGauss(image, rad1, rad2):
-    size1 = rad1 * 2 + 1
-    size2 = rad2 * 2 + 1
-    img1 = cv2.GaussianBlur(image, (size1, size1), 0)
-    img2 = cv2.GaussianBlur(image, (size2, size2), 0)
-    DoG = (img1 - img2)
-    return DoG
-
-# https://stackoverflow.com/questions/4993082/how-can-i-sharpen-an-image-in-opencv
-def unsharp_mask(image, kernel_size=(5, 5), sigma=1.0, amount=1.0, threshold=0):
-    """Return a sharpened version of the image, using an unsharp mask."""
-    blurred = cv2.GaussianBlur(image, kernel_size, sigma)
-    sharpened = float(amount + 1) * image - float(amount) * blurred
-    sharpened = np.maximum(sharpened, np.zeros(sharpened.shape))
-    sharpened = np.minimum(sharpened, 255 * np.ones(sharpened.shape))
-    sharpened = sharpened.round().astype(np.uint8)
-    if threshold > 0:
-        low_contrast_mask = np.absolute(image - blurred) < threshold
-        np.copyto(sharpened, image, where=low_contrast_mask)
-    return sharpened
-
-def image_threshold(image, lower, upper = 255):
-   
-
-    tmp = image.copy()
-
-    for height in range(tmp.shape[0]):
-        for width in range(tmp.shape[1]):
-            if tmp[height, width] < lower:
-                tmp[height, width] = 0
-            elif tmp[height, width] > upper:
-                tmp[height, width] = 0
-            else:
-                tmp[height, width] = 255
-                
-
-    return tmp
-
-# * Resize image to fit screen while keeping aspect ratio
-def ResizeToFit(oriimg, H = 980, W = 1820):
-    if(len(oriimg.shape) == 3):
-        height, width, depth = oriimg.shape
-    else:
-        height, width = oriimg.shape
-    scaleWidth = float(W)/float(width)
-    scaleHeight = float(H)/float(height)
-
-    if scaleHeight>scaleWidth:
-        imgScale = scaleWidth
-    else:
-        imgScale = scaleHeight
-
-    newX,newY = oriimg.shape[1]*imgScale, oriimg.shape[0]*imgScale
-    newimg = cv2.resize(oriimg,(int(newX),int(newY)))
-    return newimg
-
-def rotate_image(image, angle):
-    image_center = tuple(np.array(image.shape[1::-1]) / 2)
-    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
-    result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
-    return result
-
-# TODO: Lær at forstå hvordan matplot virker.
-#? Matplot giver stadig ikke mening når det kommer til Hough space.
-def HoughLinesSearchSkimage(img):
-    
-    minDist = 20
-    maxDist = 50
-    h,theta,d = hough_line(img)
-    
-    fig, axes = plt.subplots(1, 2, figsize=(15, 6))
-    ax = axes.ravel()
-
-    ax[0].imshow(img, cmap=cm.gray)
-    ax[0].set_title('Input image')
-    ax[0].set_axis_off()
-
-    angle_step = 0.5 * np.diff(theta).mean()
-    d_step = 0.5 * np.diff(d).mean()
-    bounds = [np.rad2deg(theta[0] - angle_step),
-            np.rad2deg(theta[-1] + angle_step),
-            d[-1] + d_step, d[0] - d_step]
-    ax[1].imshow(np.log(1 + h), extent=bounds, cmap=cm.gray, aspect=1 / 1.5)
-    ax[1].set_title('Hough transform')
-    ax[1].set_xlabel('Angles (degrees)')
-    ax[1].set_ylabel('Distance (pixels)')
-    ax[1].axis('image')
-
-    plt.tight_layout()
-    plt.show()
-    
-    houghImage = img
-    
-    return houghImage
-
-def HoughLinesSearch(img, houghLength=40, houghDist=10):
-    #img has to be the edge detected image.
-    #Copy of edge detected image into BGR image for drawing lines.
-    houghImage = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-    #Find HoughLines on the image. Default houghLengt = 40, houghDist=10
-    linesP = cv2.HoughLinesP(img, 1, np.pi / 180, 50, None, houghLength, houghDist)
-    #If-statement drawing lines on the copy, if any lines are found.
-    if linesP is not None:
-        sortedLines = SortLines(linesP)
-        LineGrouping = LinesGrouping(sortedLines)
-        glassSides = LineMerge(LineGrouping) 
-        b = 255
-        g = 0
-        r = 0
-        '''
-        # * For printing all lines use:
-        for i in range(0, len(linesP)): #for all lines: "linesP", for one glass all lines: "LineGrouping"
-            l = linesP[i] # same as above
-            l = l.astype(int)
-            cv2.line(houghImage, (l[0,0], l[0,1]), (l[0,2], l[0,3]), (b,g,r), 3, cv2.LINE_AA)
-            g+=-255
-            r+=255
-        '''
-        for i in range(0, len(glassSides)): #for all lines: "linesP", for one glass all lines: "LineGrouping"
-            l = glassSides[i] # same as above
-            l = l.astype(int)
-            cv2.line(houghImage, (l[1], l[2]), (l[3], l[4]), (b,g,r), 3, cv2.LINE_AA)
-            g+=-255
-            r+=255
-        print(len(glassSides),"lines found")
-    else:
-        print("No lines found")
-    return houghImage
-
-def LineMerge(glassLines):
-    # * function that merge the lines of a side to only one line
-    lineMerged = np.zeros([800,6])
-    k = 0
-    if len(glassLines) == 2: # check if there exist only 2 lines
-        a = np.array(abs(glassLines[0:2,1] - glassLines[0:2, 3]))
-        #a0 = abs(glassLines[0,1]-glassLines[0,3])
-        b0 = abs(glassLines[0,2]-glassLines[0,4])
-        #a1 = abs(glassLines[1,1]-glassLines[1,3])
-        b1 = abs(glassLines[1,2]-glassLines[1,4])
-        c0 = np.hypot(a[0],b0)
-        c1 = np.hypot(a[1],b1)
-        lineMerged[0,0:5] = glassLines[0,0:5]
-        lineMerged[1,0:5] =glassLines[1,0:5]
-        lineMerged[0,5] = c0
-        lineMerged[1,5] = c1
-    elif len(glassLines) == 3: # check if there exist only 3 lines
-        angleRangeLower = glassLines[0,0]-0.2
-        angleRangeUpper = glassLines[0,0]+0.2
-
-        x0Start,y0Start,x0End,y0End = glassLines[0,1:5]
-        x1Start,y1Start,x1End,y1End = glassLines[1,1:5]
-        x2Start,y2Start,x2End,y2End = glassLines[2,1:5]
-
-        slope01 = linregress([x0Start,x1End],[y0Start,y1End])
-        slope02 = linregress([x0Start,x2End],[y0Start,y2End])
-        slope12 = linregress([x1Start,x2End],[y1Start,y2End])
-        
-        slope01 = slope01.slope
-        slope02 = slope02.slope
-        slope12 = slope12.slope
-            
-        if (slope01 > angleRangeLower) and (slope01 < angleRangeUpper):
-            a1 = abs(glassLines[0,1]-glassLines[1,3])
-            b1 = abs(glassLines[0,2]-glassLines[1,4])
-            c1 = np.hypot(a1,b1)
-            a2 = abs(glassLines[1,1]-glassLines[0,3])
-            b2 = abs(glassLines[1,2]-glassLines[0,4])
-            c2 = np.hypot(a2,b2)
-            
-            if c1 > c2:
-                lineMerged[0,0],lineMerged[0,1],lineMerged[0,2],lineMerged[0,3],lineMerged[0,4],lineMerged[0,5] = slope01,x0Start,y0Start,x1End,y1End,c1
-            else:
-                lineMerged[0,0],lineMerged[0,1],lineMerged[0,2],lineMerged[0,3],lineMerged[0,4],lineMerged[0,5] = slope01,x1Start,y1Start,x0End,y0End,c2
-            
-            a = abs(glassLines[2,1]-glassLines[2,3])
-            b = abs(glassLines[2,2]-glassLines[2,4])
-            c = np.hypot(a,b)
-            lineMerged[1,0],lineMerged[1,1],lineMerged[1,2],lineMerged[1,3],lineMerged[1,4],lineMerged[1,5] = glassLines[2,0],x2Start,y2Start,x2End,y2End,c
-
-            
-        elif (slope02 > angleRangeLower and slope02 < angleRangeUpper):
-            a1 = abs(glassLines[0,1]-glassLines[2,3])
-            b1 = abs(glassLines[0,2]-glassLines[2,4])
-            c1 = np.hypot(a1,b1)
-            a2 = abs(glassLines[2,1]-glassLines[0,3])
-            b2 = abs(glassLines[2,2]-glassLines[0,4])
-            c2 = np.hypot(a2,b2)
-            
-            if c1 > c2:
-                lineMerged[0,0],lineMerged[0,1],lineMerged[0,2],lineMerged[0,3],lineMerged[0,4],lineMerged[0,5] = slope02,x0Start,y0Start,x2End,y2End,c1
-
-            else:
-                lineMerged[0,0],lineMerged[0,1],lineMerged[0,2],lineMerged[0,3],lineMerged[0,4],lineMerged[0,5] = slope02,x2Start,y2Start,x0End,y0End,c2
-            
-            a = abs(glassLines[1,1]-glassLines[1,3])
-            b = abs(glassLines[1,2]-glassLines[1,4])
-            c = np.hypot(a,b)
-            lineMerged[1,0],lineMerged[1,1],lineMerged[1,2],lineMerged[1,3],lineMerged[1,4],lineMerged[1,5] = glassLines[1,0],x1Start,y1Start,x1End,y1End,c
-            
-        else:
-            a1 = abs(glassLines[1,1]-glassLines[2,3])
-            b1 = abs(glassLines[1,2]-glassLines[2,4])
-            c1 = np.hypot(a1,b1)
-            a2 = abs(glassLines[2,1]-glassLines[1,3])
-            b2 = abs(glassLines[2,2]-glassLines[1,4])
-            c2 = np.hypot(a2,b2)
-            
-            if c1 > c2:
-                lineMerged[0,0],lineMerged[0,1],lineMerged[0,2],lineMerged[0,3],lineMerged[0,4],lineMerged[0,5] = slope12,x1Start,y1Start,x2End,y2End,c1
-            else:
-                lineMerged[0,0],lineMerged[0,1],lineMerged[0,2],lineMerged[0,3],lineMerged[0,4],lineMerged[0,5] = slope12,x1Start,y1Start,x1End,y1End,c2
-            
-            a = abs(glassLines[0,1]-glassLines[0,3])
-            b = abs(glassLines[0,2]-glassLines[0,4])
-            c = np.hypot(a,b)
-            lineMerged[1,0],lineMerged[1,1],lineMerged[1,2],lineMerged[1,3],lineMerged[1,4],lineMerged[1,5] = glassLines[0,0],x0Start,y0Start,x0End,y0End,c
-
-            
-    else: # For 3+ lines
-        for i in range(0,len(glassLines)):
-            for j in range(i,len(glassLines)):
-                if j == i:
-                    continue
-                a = abs(glassLines[i,1]-glassLines[j,3])
-                b = abs(glassLines[i,2]-glassLines[j,4])
-
-                coordinates = np.array(np.r_[glassLines[i,1:3], glassLines[j, 3:5]]) #xstart ystart xend yend
-
-                angleRangeLower = glassLines[i,0]-angleTolerance
-                angleRangeUpper = glassLines[i,0]+angleTolerance
-                
-                slope = linregress([coordinates[0], coordinates[2]], [coordinates[1], coordinates[3]])
-                if slope.slope > angleRangeLower and slope.slope < angleRangeUpper:
-                    c = np.hypot(a,b)
-                    
-                    lineMerged[k,0] = slope.slope
-                    lineMerged[k,1:5] = coordinates
-                    lineMerged[k,5] = c
-                    k+=1
-    return lineMerged
-
-def LinesGrouping(sortedLines):
-    # * function that sorts the sorted lines into each glass, again using the slopes
-    value = None
-    lineGroup = []
-    range_upper = 0
-    k = 0
-    glass = np.zeros([1200, 5])
-    sortedLinesArray = np.array(sortedLines)
-    np.set_printoptions(precision=6,suppress=True)
-    
-    # for-loop to determind the range of allowed difference in slope            
-    for i in range(0,len(sortedLinesArray)):
-        if value is None:
-            value=sortedLinesArray[0,0]
-            range_upper=value+0.45
-        elif range_upper < sortedLinesArray[i,0]:
-            break
-        glass[k, 0:5] = sortedLinesArray[i, 0:5]
-        k += 1
-    lineGroup=glass
-    
-    # deletion of zero rows
-    for i in range(len(lineGroup)-1,0,-1):
-        if ((lineGroup[i,1] == 0) and (lineGroup[i,2] == 0) and (lineGroup[i,3] == 0) and (lineGroup[i,4] == 0)):
-            lineGroup = np.delete(lineGroup, (i), axis=0)
-    return lineGroup
-
-def SortLines(linesP):
-    # * function that sorts the found lines after gradient
-    slope_sorted=[0 for i in range(len(linesP))]
-    for i in range(0, len(linesP)):
-        l = linesP[i][0]
-        x = [l[0], l[2]]
-        y = [l[1], l[3]]
-        slope = linregress(x,y)
-        # ! dorment function that can correct for full revolutions if this happens. Remember to activate by adding the +/- comments
-        if slope.slope > 3.1415:
-            slope_fixed = slope.slope#-(2*3.1415)
-        elif slope.slope < -3.1415:
-            slope_fixed = slope.slope#+(2*3.1415)
-        else:
-            slope_fixed = slope.slope
-        # *               slope         x1   y1     x2   y2
-        slope_sorted[i] = [slope_fixed,l[0],l[1],l[2],l[3]]
-    slope_sorted.sort(key=lambda x:x[0])
-    sortedLines = slope_sorted
-    
-    return sortedLines
 
 #**********************Main loop********************************
 
@@ -335,32 +31,32 @@ cv2.destroyAllWindows()
 '''
 #**************FOR FREDERIK'S VISUAL IMAGE USE:*****************
 # * Chain should be:
-# * Crop -> Resize to (H,W = 303, 450) -> Cvt to gray -> ...
-# * Hist. stretch -> Blur (rad. = (3,3), SigmaX = 1.5) -> ...
-# * Unsharp mask (Size (3,3), amount 1.2, thresh. .131) -> ...
-# * Laplacian edge (delta = 5.3) -> Cvt. to UByte -> ...
+# * Crop -> Resize to (H,W = 403, 550) -> Cvt to gray -> ...
+# * Hist. stretch -> Blur (rad. = (3,3), SigmaX = 7) -> ...
+# * Unsharp mask (Size (3,3), amount 1, thresh. .131) -> ...
+# * Laplacian edge (delta = 5) -> Cvt. to UByte -> ...
 # * Threshold @ 30
-
-img = cv2.imread('opencv_frame_5.png')
 start_time = time.time()
 
+img = cv2.imread('opencv_frame_2.png')
+
 img_cropped = img[60:60+505, 325:325+740]
-img_screensized = ResizeToFit(img_cropped, H= 303, W = 450)
+img_screensized = prep.ResizeToFit(img_cropped, H= 403, W = 550)
 img_gray = cv2.cvtColor(img_screensized, cv2.COLOR_BGR2GRAY)
 img_stretched = cv2.equalizeHist(img_gray)
 img_blur = cv2.GaussianBlur(img_stretched, (3,3), 7)
-img_sharpen = unsharp_mask(img_blur, kernel_size = (3,3), amount = 1, threshold = 0.131)
+img_sharpen = prep.unsharp_mask(img_blur, kernel_size = (3,3), amount = 1, threshold = 0.131)
 img_edges = cv2.Laplacian(img_sharpen, ddepth = cv2.CV_16S, delta = 5)
 img_edges = img_as_ubyte(img_edges)
-img_binary = cv2.threshold(img_edges, 30, maxval = 255, type = cv2.THRESH_BINARY)
+# ! Consider using Otsu's binarization THRESH_OTSU instead
+img_binary = cv2.threshold(img_edges, 30, maxval = 255, type = cv2.THRESH_OTSU)
 img_binary = img_binary[1]
-#upscaled = ResizeToFit(img_binary, H = 720, W = 1080)
+#upscaled = prep.ResizeToFit(img_binary, H = 720, W = 1080)
+#cv2.imshow('Preprocessing', upscaled)
+
+edges_hough = ls.HoughLinesSearch(img_binary)
 print("--- %s seconds ---" % (time.time()-start_time))
 
-
-
-
-edges_hough = HoughLinesSearch(img_binary)
 cv2.imshow('edge_hough',edges_hough)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
---
+++ GlassSegmentV1/LineSearchLib.py
@@ -0,0 +1,235 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import linregress
+
+#*********** GLOBAL PARAMETERS **************
+angleTolerance = 0.3
+
+#********************************************
+
+def SortLines(linesP):
+    # * function that sorts the found lines after gradient
+    slope_sorted=[0 for i in range(len(linesP))]
+    for i in range(0, len(linesP)):
+        l = linesP[i][0]
+        x = [l[0], l[2]]
+        y = [l[1], l[3]]
+        slope = linregress(x,y)
+        # ! dorment function that can correct for full revolutions if this happens. Remember to activate by adding the +/- comments
+        if slope.slope > 3.1415:
+            slope_fixed = slope.slope#-(2*3.1415)
+        elif slope.slope < -3.1415:
+            slope_fixed = slope.slope#+(2*3.1415)
+        else:
+            slope_fixed = slope.slope
+        # *               slope         x1   y1     x2   y2
+        slope_sorted[i] = [slope_fixed,l[0],l[1],l[2],l[3]]
+    slope_sorted.sort(key=lambda x:x[0])
+    sortedLines = slope_sorted
+    
+    return sortedLines
+
+def LinesGrouping(sortedLines):
+    # * function that sorts the sorted lines into each glass, again using the slopes
+    value = None
+    lineGroup = []
+    range_upper = 0
+    k = 0
+    glass = np.zeros([1200, 5])
+    sortedLinesArray = np.array(sortedLines)
+    np.set_printoptions(precision=6,suppress=True)
+    
+    # for-loop to determind the range of allowed difference in slope            
+    for i in range(0,len(sortedLinesArray)):
+        if value is None:
+            value=sortedLinesArray[0,0]
+            range_upper=value+0.45
+        elif range_upper < sortedLinesArray[i,0]:
+            break
+        glass[k, 0:5] = sortedLinesArray[i, 0:5]
+        k += 1
+    lineGroup=glass
+    
+    # deletion of zero rows
+    for i in range(len(lineGroup)-1,0,-1):
+        if ((lineGroup[i,1] == 0) and (lineGroup[i,2] == 0) and (lineGroup[i,3] == 0) and (lineGroup[i,4] == 0)):
+            lineGroup = np.delete(lineGroup, (i), axis=0)
+    return lineGroup
+
+def LineMerge(glassLines):
+    # * function that merge the lines of a side to only one line
+    lineMerged = np.zeros([800,6])
+    k = 0
+    if len(glassLines) == 2: # check if there exist only 2 lines
+        a = np.array(abs(glassLines[0:2,1] - glassLines[0:2, 3]))
+        #a0 = abs(glassLines[0,1]-glassLines[0,3])
+        b0 = abs(glassLines[0,2]-glassLines[0,4])
+        #a1 = abs(glassLines[1,1]-glassLines[1,3])
+        b1 = abs(glassLines[1,2]-glassLines[1,4])
+        c0 = np.hypot(a[0],b0)
+        c1 = np.hypot(a[1],b1)
+        lineMerged[0,0:5] = glassLines[0,0:5]
+        lineMerged[1,0:5] =glassLines[1,0:5]
+        lineMerged[0,5] = c0
+        lineMerged[1,5] = c1
+    elif len(glassLines) == 3: # check if there exist only 3 lines
+        angleRangeLower = glassLines[0,0]-0.2
+        angleRangeUpper = glassLines[0,0]+0.2
+
+        x0Start,y0Start,x0End,y0End = glassLines[0,1:5]
+        x1Start,y1Start,x1End,y1End = glassLines[1,1:5]
+        x2Start,y2Start,x2End,y2End = glassLines[2,1:5]
+
+        slope01 = linregress([x0Start,x1End],[y0Start,y1End])
+        slope02 = linregress([x0Start,x2End],[y0Start,y2End])
+        slope12 = linregress([x1Start,x2End],[y1Start,y2End])
+        
+        slope01 = slope01.slope
+        slope02 = slope02.slope
+        slope12 = slope12.slope
+            
+        if (slope01 > angleRangeLower) and (slope01 < angleRangeUpper):
+            a1 = abs(glassLines[0,1]-glassLines[1,3])
+            b1 = abs(glassLines[0,2]-glassLines[1,4])
+            c1 = np.hypot(a1,b1)
+            a2 = abs(glassLines[1,1]-glassLines[0,3])
+            b2 = abs(glassLines[1,2]-glassLines[0,4])
+            c2 = np.hypot(a2,b2)
+            
+            if c1 > c2:
+                lineMerged[0,0],lineMerged[0,1],lineMerged[0,2],lineMerged[0,3],lineMerged[0,4],lineMerged[0,5] = slope01,x0Start,y0Start,x1End,y1End,c1
+            else:
+                lineMerged[0,0],lineMerged[0,1],lineMerged[0,2],lineMerged[0,3],lineMerged[0,4],lineMerged[0,5] = slope01,x1Start,y1Start,x0End,y0End,c2
+            
+            a = abs(glassLines[2,1]-glassLines[2,3])
+            b = abs(glassLines[2,2]-glassLines[2,4])
+            c = np.hypot(a,b)
+            lineMerged[1,0],lineMerged[1,1],lineMerged[1,2],lineMerged[1,3],lineMerged[1,4],lineMerged[1,5] = glassLines[2,0],x2Start,y2Start,x2End,y2End,c
+
+            
+        elif (slope02 > angleRangeLower and slope02 < angleRangeUpper):
+            a1 = abs(glassLines[0,1]-glassLines[2,3])
+            b1 = abs(glassLines[0,2]-glassLines[2,4])
+            c1 = np.hypot(a1,b1)
+            a2 = abs(glassLines[2,1]-glassLines[0,3])
+            b2 = abs(glassLines[2,2]-glassLines[0,4])
+            c2 = np.hypot(a2,b2)
+            
+            if c1 > c2:
+                lineMerged[0,0],lineMerged[0,1],lineMerged[0,2],lineMerged[0,3],lineMerged[0,4],lineMerged[0,5] = slope02,x0Start,y0Start,x2End,y2End,c1
+
+            else:
+                lineMerged[0,0],lineMerged[0,1],lineMerged[0,2],lineMerged[0,3],lineMerged[0,4],lineMerged[0,5] = slope02,x2Start,y2Start,x0End,y0End,c2
+            
+            a = abs(glassLines[1,1]-glassLines[1,3])
+            b = abs(glassLines[1,2]-glassLines[1,4])
+            c = np.hypot(a,b)
+            lineMerged[1,0],lineMerged[1,1],lineMerged[1,2],lineMerged[1,3],lineMerged[1,4],lineMerged[1,5] = glassLines[1,0],x1Start,y1Start,x1End,y1End,c
+            
+        else:
+            a1 = abs(glassLines[1,1]-glassLines[2,3])
+            b1 = abs(glassLines[1,2]-glassLines[2,4])
+            c1 = np.hypot(a1,b1)
+            a2 = abs(glassLines[2,1]-glassLines[1,3])
+            b2 = abs(glassLines[2,2]-glassLines[1,4])
+            c2 = np.hypot(a2,b2)
+            
+            if c1 > c2:
+                lineMerged[0,0],lineMerged[0,1],lineMerged[0,2],lineMerged[0,3],lineMerged[0,4],lineMerged[0,5] = slope12,x1Start,y1Start,x2End,y2End,c1
+            else:
+                lineMerged[0,0],lineMerged[0,1],lineMerged[0,2],lineMerged[0,3],lineMerged[0,4],lineMerged[0,5] = slope12,x1Start,y1Start,x1End,y1End,c2
+            
+            a = abs(glassLines[0,1]-glassLines[0,3])
+            b = abs(glassLines[0,2]-glassLines[0,4])
+            c = np.hypot(a,b)
+            lineMerged[1,0],lineMerged[1,1],lineMerged[1,2],lineMerged[1,3],lineMerged[1,4],lineMerged[1,5] = glassLines[0,0],x0Start,y0Start,x0End,y0End,c
+
+            
+    else: # For 3+ lines
+        for i in range(0,len(glassLines)):
+            for j in range(i,len(glassLines)):
+                if j == i:
+                    continue
+                a = abs(glassLines[i,1]-glassLines[j,3])
+                b = abs(glassLines[i,2]-glassLines[j,4])
+
+                coordinates = np.array(np.r_[glassLines[i,1:3], glassLines[j, 3:5]]) #xstart ystart xend yend
+
+                angleRangeLower = glassLines[i,0]-angleTolerance
+                angleRangeUpper = glassLines[i,0]+angleTolerance
+                
+                slope = linregress([coordinates[0], coordinates[2]], [coordinates[1], coordinates[3]])
+                if slope.slope > angleRangeLower and slope.slope < angleRangeUpper:
+                    c = np.hypot(a,b)
+                    
+                    lineMerged[k,0] = slope.slope
+                    lineMerged[k,1:5] = coordinates
+                    lineMerged[k,5] = c
+                    k+=1
+    return lineMerged
+
+def HoughLinesSearch(img, houghLength=40, houghDist=10):
+    #img has to be the edge detected image.
+    #Copy of edge detected image into BGR image for drawing lines.
+    houghImage = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+    #Find HoughLines on the image. Default houghLengt = 40, houghDist=10
+    linesP = cv2.HoughLinesP(img, 1, np.pi / 180, 50, None, houghLength, houghDist)
+    #If-statement drawing lines on the copy, if any lines are found.
+    if linesP is not None:
+        sortedLines = SortLines(linesP)
+        LineGrouping = LinesGrouping(sortedLines)
+        glassSides = LineMerge(LineGrouping) 
+        b = 255
+        g = 0
+        r = 0
+        '''
+        # * For printing all lines use:
+        for i in range(0, len(linesP)): #for all lines: "linesP", for one glass all lines: "LineGrouping"
+            l = linesP[i] # same as above
+            l = l.astype(int)
+            cv2.line(houghImage, (l[0,0], l[0,1]), (l[0,2], l[0,3]), (b,g,r), 3, cv2.LINE_AA)
+            g+=-255
+            r+=255
+        '''
+        for i in range(0, len(glassSides)): #for all lines: "linesP", for one glass all lines: "LineGrouping"
+            l = glassSides[i] # same as above
+            l = l.astype(int)
+            cv2.line(houghImage, (l[1], l[2]), (l[3], l[4]), (b,g,r), 3, cv2.LINE_AA)
+            g+=-255
+            r+=255
+        print(len(glassSides),"lines found")
+    else:
+        print("No lines found")
+    return houghImage
+
+def HoughLinesSearchSkimage(img):
+    
+    minDist = 20
+    maxDist = 50
+    h,theta,d = hough_line(img)
+    
+    fig, axes = plt.subplots(1, 2, figsize=(15, 6))
+    ax = axes.ravel()
+
+    ax[0].imshow(img, cmap=cm.gray)
+    ax[0].set_title('Input image')
+    ax[0].set_axis_off()
+
+    angle_step = 0.5 * np.diff(theta).mean()
+    d_step = 0.5 * np.diff(d).mean()
+    bounds = [np.rad2deg(theta[0] - angle_step),
+            np.rad2deg(theta[-1] + angle_step),
+            d[-1] + d_step, d[0] - d_step]
+    ax[1].imshow(np.log(1 + h), extent=bounds, cmap=cm.gray, aspect=1 / 1.5)
+    ax[1].set_title('Hough transform')
+    ax[1].set_xlabel('Angles (degrees)')
+    ax[1].set_ylabel('Distance (pixels)')
+    ax[1].axis('image')
+
+    plt.tight_layout()
+    plt.show()
+    
+    houghImage = img
+    
+    return houghImage
