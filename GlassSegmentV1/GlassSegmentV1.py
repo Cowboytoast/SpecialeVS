748f2d212cfe68ade5c12ca660e228f05432b055
Frederik Knudsen <frederik.s.k@hotmail.com>
2021-03-25 13:19:06 +0100

    Cleanup

--- GlassSegmentV1/GlassSegmentV1.py
+++ GlassSegmentV1/GlassSegmentV1.py
@@ -22,25 +22,8 @@ def ResizeToFit(oriimg):
     newimg = cv2.resize(oriimg,(int(newX),int(newY)))
     return newimg
 
-# ! Unused due to bounding-box non-scaliness. Use rotate_bound instead
-def rotate_image(image, angle):
-    image_center = tuple(np.array(image.shape[1::-1]) / 2)
-    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
-    result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_CUBIC)
-    return result
-
-# ! An (likely) ineffective way to convert image to binary
-def cvt_to_bin(image):
-    image_bin = np.zeros(image.shape,dtype=bool)
-    for y in range(image.shape[0]):
-        for x in range(image.shape[1]):
-            if image[y,x] == 255:
-                image_bin[y,x] = True
-    return image_bin
-
-def templatematch(img, template, houghLocation, h_steps = 10, w_steps = 10):
+def templatematch(img, template, houghLocation, h_steps = 20, w_steps = 20):
     # * line-pair = |slope1 = a rad | x1start | y1start | x1end | y1end | slope2 = b rad | x2start | y2start | x2end | y2end
-
     pointsx = np.array([houghLocation[1], houghLocation[3], houghLocation[6], houghLocation[8]])
     pointsy = np.array([houghLocation[2], houghLocation[4], houghLocation[7], houghLocation[9]])
     slopes = np.array([math.degrees(houghLocation[0]), math.degrees(houghLocation[5])])
@@ -49,19 +32,15 @@ def templatematch(img, template, houghLocation, h_steps = 10, w_steps = 10):
     for cnt in range(slopes.shape[0]):
         if np.sign(slopes[cnt]) == -1:
             slopes[cnt] += 360
-          
-            
+
     slope_offset = np.average(slopes)
     template_rot = imutils.rotate_bound(template, slope_offset)
     pointsy -= int(template_rot.shape[0]/2)
     pointsx -= int(template_rot.shape[1]/4)
-    #matches = np.empty([int(10/angle_inc), img.shape[0] - template_rot.shape[1] + 10, img.shape[1] - template_rot.shape[1] + 10])
     
-    maxval = 0
-
     # Do & operation in increments, that is moving the template image a few pixels right/down
     # for each iteration and store most pixel hits
-    
+    maxval = 0
     UpDown = 1 # 1 for up, 0 for down
     
     for h in np.arange(int(np.amin(pointsy)) - int(h_steps/2), int(np.amin(pointsy)) + int(h_steps/2), 1):
@@ -72,18 +51,14 @@ def templatematch(img, template, houghLocation, h_steps = 10, w_steps = 10):
             if matches > maxval:
                 maxval = matches
                 max_idx = np.array([h, w])
-            #template_rot = imutils.rotate_bound(template, angle)
             #rotatingim = np.copy(img)
-            #rotatingim[h : h + templateTop_rot.shape[0], w : w + templateTop_rot.shape[1]] = templateTop_rot
+            #rotatingim[h : h + template_rot.shape[0], w : w + template_rot.shape[1]] = template_rot
             #cv2.imshow('Rotating progress', rotatingim)
-            #cv2.waitKey(1)
-                #matches[int(angle/angle_inc), h, w] = np.count_nonzero(match_array)
+            #cv2.waitKey(10)
     slope_offset += 180
     template_rot = imutils.rotate_bound(template, slope_offset)
     UpDown = 0 # 1 for up, 0 for down
 
-
-    #max_rot = int(max_idx[0])
     max_h = int(max_idx[0])
     max_w = int(max_idx[1])
     overlay = cv2.imread('VialOutline.png', 0)
@@ -96,8 +71,6 @@ start_time = time.time()
 
 # * Load image and convert to binary
 img = cv2.imread('Frame4_Prep.png', 0)
-#img = ResizeToFit(img)
-#img_bin = cvt_to_bin(img)
 
 # * Load template and convert to binary
 template = cv2.imread('VialTop.png', 0)
@@ -107,12 +80,11 @@ template = cv2.imread('VialTop.png', 0)
 
 houghLocation = np.array([math.radians(343), 186, 56, 225, 185, math.radians(343), 208, 51, 247, 179])
 final = templatematch(img, template, houghLocation)
+cv2.imwrite('TemplateResult.png', final)
 print("--- %s seconds ---" % (time.time() - start_time))
-# TODO Prepare to function with line coordinates to narrow down position
+
 # TODO Function with Mathias' program which sends:
 # TODO line-pair = |slope1 = a rad | x1start | y1start | x1end | y1end | slope2 = b rad | x2start | y2start | x2end | y2end
-# TODO Search in the point-wise vicinity, search within , e.g +- 10 deg, turn 180 deg, search again within +-10 deg, use best fit
-cv2.imshow('bla1', img)
 cv2.imshow('bla', final)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
