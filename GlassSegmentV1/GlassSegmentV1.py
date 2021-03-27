c10c7dc7ffa9d93245af840c4bab0e3c1f479fd2
Frederik Knudsen <frederik.s.k@hotmail.com>
2021-03-27 16:29:09 +0100

    Started merge of template matching

--- GlassSegmentV1/GlassSegmentV1.py
+++ GlassSegmentV1/GlassSegmentV1.py
@@ -5,11 +5,60 @@ import PreprocessingLib as prep
 from skimage.util import img_as_ubyte
 import numpy as np
 
+import math
+import imutils
+from scipy import ndimage
+
 #*********** GLOBAL PARAMETERS **************
 angleTolerance = 0.3
 
 #********************************************
 
+def templatematch(img, template, houghLocation, h_steps = 20, w_steps = 20):
+    # * line-pair = |slope1 = a rad | x1start | y1start | x1end | y1end | slope2 = b rad | x2start | y2start | x2end | y2end
+    pointsx = np.array([houghLocation[1], houghLocation[3], houghLocation[6], houghLocation[8]])
+    pointsy = np.array([houghLocation[2], houghLocation[4], houghLocation[7], houghLocation[9]])
+    slopes = np.array([math.degrees(houghLocation[0]), math.degrees(houghLocation[5])])
+
+    # Convert to positive slope angle
+    for cnt in range(slopes.shape[0]):
+        if np.sign(slopes[cnt]) == -1:
+            slopes[cnt] += 360
+
+    slope_offset = np.average(slopes)
+    template_rot = imutils.rotate_bound(template, slope_offset)
+    pointsy -= int(template_rot.shape[0]/2)
+    pointsx -= int(template_rot.shape[1]/4)
+    
+    # Do & operation in increments, that is moving the template image a few pixels right/down
+    # for each iteration and store most pixel hits
+    maxval = 0
+    UpDown = 1 # 1 for up, 0 for down
+    
+    for h in np.arange(int(np.amin(pointsy)) - int(h_steps/2), int(np.amin(pointsy)) + int(h_steps/2), 1):
+        for w in np.arange(int(np.amin(pointsx)) - int(w_steps/2), int(np.amin(pointsx)) + int(w_steps/2), 1):
+            matches = np.logical_and(img[h : h + template_rot.shape[0], w : w + template_rot.shape[1]], template_rot)
+            matches = np.count_nonzero(matches)
+            
+            if matches > maxval:
+                maxval = matches
+                max_idx = np.array([h, w])
+            #rotatingim = np.copy(img)
+            #rotatingim[h : h + template_rot.shape[0], w : w + template_rot.shape[1]] = template_rot
+            #cv2.imshow('Rotating progress', rotatingim)
+            #cv2.waitKey(10)
+    slope_offset += 180
+    template_rot = imutils.rotate_bound(template, slope_offset)
+    UpDown = 0 # 1 for up, 0 for down
+
+    max_h = int(max_idx[0])
+    max_w = int(max_idx[1])
+    overlay = cv2.imread('VialOutline.png', 0)
+    detection = imutils.rotate_bound(overlay, np.average(slopes))
+    final = img
+    final[max_h : max_h + detection.shape[0], max_w : max_w+detection.shape[1]] += detection
+    
+    return final
 
 #**********************Main loop********************************
 
@@ -39,6 +88,7 @@ cv2.destroyAllWindows()
 start_time = time.time()
 
 img = cv2.imread('opencv_frame_2.png')
+template = cv2.imread('VialTop.png', 0) # * Load template
 
 img_cropped = img[60:60+505, 325:325+740]
 img_screensized = prep.ResizeToFit(img_cropped, H= 403, W = 550)
@@ -55,9 +105,24 @@ img_binary = img_binary[1]
 #cv2.imshow('Preprocessing', upscaled)
 
 edges_hough = ls.HoughLinesSearch(img_binary)
-print("--- %s seconds ---" % (time.time()-start_time))
+#print("--- %s seconds ---" % (time.time()-start_time))
 
-cv2.imshow('edge_hough',edges_hough)
+#cv2.imshow('edge_hough',edges_hough)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+#***************************************************************
+
+
+
+# * Rotating template in increments
+# * and match with image patch
+
+houghLocation = np.array([math.radians(343), 186, 56, 225, 185, math.radians(343), 208, 51, 247, 179])
+final = templatematch(img_binary, template, houghLocation)
+print("--- %s seconds ---" % (time.time() - start_time))
+
+# TODO Function with Mathias' program which sends:
+# TODO line-pair = |slope1 = a rad | x1start | y1start | x1end | y1end | slope2 = b rad | x2start | y2start | x2end | y2end
+cv2.imshow('bla', final)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-#***************************************************************
