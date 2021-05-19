76a723a4ce6d3f8f1c513642706d8444366c1f5a
MathiasBadstue <s153096@student.dtu.dk>
2021-05-19 13:31:58 +0200

    Further work on robotlib, added look up table

--- GlassSegmentV1/RobotLib.py
+++ GlassSegmentV1/RobotLib.py
@@ -13,6 +13,9 @@ robotfunc = Robot()
 gripperfunc = Gripper()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# TODO: lav init korrekt
+# TODO: Lav funktion der kører alle de forskellige funktioner i forbindelse med bevægelse af robot, denne skal så kaldes i main
+
 
 def robotInit():
     #! NEEDS LOT OF FIXING!!!!!!
@@ -25,7 +28,7 @@ def robotInit():
     return 1
 
 
-
+#* Function to move the robot arm from 'waiting' position, to a vial and pick it up. This does not lift it as such, just takes it into the grabber.
 def moveCommand(x,y,z,rx,ry,rz):
     moveComplete = False
     t=robot.transform(x,y,z) #* Generate placement of the glass in robot frame
@@ -37,8 +40,10 @@ def moveCommand(x,y,z,rx,ry,rz):
     
     return moveComplete
 
+
+#* Function that generates and sends a command to move to handoff placement of vials. Uses an array with x,y,z (for the transform) and rx,ry,rz with the angles.
+#* The array are generated in the function 'handOffPosLOT'.
 def handoffCommand():
-    #* Generate and send command to move to handoff placement of vials. Uses an array with x,y,z (for the transform) and rx,ry,rz with the angles.
     
     #! handoffpos needs to be adjudsted with 16mm per turn, with a max of 16. If we are smart, lay it out so we only have to adjust in one direction.
     #? Should we make a global counter, or should we make an internal counter that is passed to maintain its value?
@@ -47,7 +52,6 @@ def handoffCommand():
     
     handOffPos = handOffPosLOT()
     
-    t=robot.transform(handOffPos[0,extractCounter],handOffPos[1,extractCounter],handOffPos[2,extractCounter]) #* Generate placement of the glass in robot frame
     s.send('movel(p['+str(t[0])+','+str(t[1])+','+str(t[2])+','+str(handOffPos[3])+','+str(handOffPos[4,extractCounter])+','+str(handOffPos[5,extractCounter])+'],1,0.1)\n')
     robotfunc.wait()
     handOffComplete = True
@@ -56,13 +60,18 @@ def handoffCommand():
     
     return handOffComplete
 
+
+#* Function to generate and move the robot to its waiting position / start-end position
+#! Vi kan måske slette denne ved brug af Trajectory Planning (se evt. robot projekt rapport)
 def waitPos(x=0,y=0,z=0,rx=0,ry=0,rz=3.14):
-    #* Generate and move the robot to its waiting position / start-end position
     t=robot.transform(x,y,z) #* Generate placement of the glass in robot frame
     s.send('movel(p['+str(t[0])+','+str(t[1])+','+str(t[2])+','+str(rx)+','+str(ry)+','+str(rz)+'],1,0.1)\n')
     robotfunc.wait()
     return 1
 
+#* The function creates a Look-up Table since we have a finite number of specific places. This is utilized to speed up the program such that the positions only have to be calculated
+#* at the beginning of the program, thus adding the least amount of time once the recognition and movement of the vials are begun. This function should only be called once
+#* after init.
 def handOffPosLOT():
     
     handOffPos = np.empty(np.shape(6,16),dtype='object')
@@ -80,7 +89,13 @@ def handOffPosLOT():
         handOffPos[i,4] = 0 #* ry
         handOffPos[i,5] = 0 #* rz
         # handoff angles does not change, thus assigned with constants.
-        
+    # moves the coordinates from world frame to robot/base frame. 
+    for m in range(0,15):
+        t=robot.transform(handOffPos[0,m],handOffPos[1,m],handOffPos[2,m]) #* Generate placement of the glass in robot frame
+        handOffPos[0,m] = t[0]
+        handOffPos[1,m] = t[1]
+        handOffPos[2,m] = t[2]
+
         #! All numbers above are only as templates as no position are defined yet.
     
     return handOffPos
