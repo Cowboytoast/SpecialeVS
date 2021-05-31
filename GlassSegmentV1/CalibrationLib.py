6d1b7708bd2db1e5bb8059aa2fdc80af6214b833
MathiasBadstue <s153096@student.dtu.dk>
2021-05-31 11:39:52 +0200

    Added online generated markers

--- GlassSegmentV1/CalibrationLib.py
+++ GlassSegmentV1/CalibrationLib.py
@@ -1,6 +1,9 @@
 import numpy as np
+import sys
 import cv2
 from cv2 import aruco
+sys.path.append("./images/ArUco")
+
 
 #* Library for calibration between camera and box/world frame
 #* Here we use ArUco markers to determind the corners of the box, corresponding to P0, Px and Py needed for the transformation between world frame and robot frame.
