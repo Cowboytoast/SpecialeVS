7064f5dc7c82d56dd36bb9666d425034f34f2744
MathiasBadstue <s153096@student.dtu.dk>
2021-06-02 12:50:43 +0200

    Markar calib fix further changes pending

--- GlassSegmentV1/CalibrationLib.py
+++ GlassSegmentV1/CalibrationLib.py
@@ -1,11 +1,18 @@
-import numpy as np
-import sys
 import cv2
 from cv2 import aruco
-sys.path.append("./images/ArUco")
 
 
 #* Library for calibration between camera and box/world frame
 #* Here we use ArUco markers to determind the corners of the box, corresponding to P0, Px and Py needed for the transformation between world frame and robot frame.
 
-
+def markerCalib(img):
+    
+    dict = cv2.aruco.Dictionary_get(aruco.DICT_4X4_50)
+    arucoParams = cv2.aruco.DetectorParameters_create()
+    corners,ids,refect = cv2.aruco.detectMarkers(img,dict,parameters=arucoParams)
+    imgMarkers = cv2.aruco.drawDetectedMarkers(img.copy(),corners,ids)
+    
+    cv2.namedWindow("markers")
+    cv2.imshow(imgMarkers,"markers")
+    
+    return corners,ids
